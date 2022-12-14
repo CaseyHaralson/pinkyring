0a4876275e9ff7dae9532a8740bef872e79abbb8
Casey Haralson <casey.haralson@gmail.com>
2022-12-13 20:46:51 -0700

    asdf

--- dist/template-helper.js
+++ dist/template-helper.js
@@ -1,11 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+//import ejs from 'ejs';
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.render = void 0;
-const ejs_1 = __importDefault(require("ejs"));
 function render(content, data) {
-    return ejs_1.default.render(content, data);
+    if (content) {
+        const simpleTemplateName = data.selectedTemplate
+            .replaceAll('-', '')
+            .replaceAll('_', '');
+        let newContent = content.replaceAll(data.selectedTemplate, data.projectName);
+        newContent = newContent.replaceAll(simpleTemplateName, data.projectName);
+        return newContent;
+    }
+    return content;
+    //return ejs.render(content, data);
 }
 exports.render = render;
